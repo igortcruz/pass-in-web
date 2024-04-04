8e1f782033e426c27a72c2851fc1887876263d56
igortcruz <igortorres213@gmail.com>
2024-04-04 08:59:05 -0300

    feat: Finalização aula 1, estrutura básica.

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -7,7 +7,7 @@ export function AttendeeList() {
             <h1 className="text-2xl font-bold">Participantes</h1>
             <div className="px-3 py-1.5 w-72 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                <Search className="size-4 text-emerald-300"/>
-               <input className="bg-transparent flex-1 outline-none" placeholder="Buscar participante..." />
+               <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
             </div>
          </div>
 
@@ -16,7 +16,7 @@ export function AttendeeList() {
                <thead>
                   <tr className="border-b border-white/10">
                      <th style={{width: 48}} className="py-3 px-4 text-sm font-semibold text-left">
-                        <input type="checkbox" />
+                        <input type="checkbox" className="size-4 bg-black/20 border border-white/10 rounded" />
                      </th>
                      <th className="py-3 px-4 text-sm font-semibold text-left">Código</th>
                      <th className="py-3 px-4 text-sm font-semibold text-left">Participante</th>
@@ -26,11 +26,11 @@ export function AttendeeList() {
                   </tr>
                </thead>
                <tbody>
-                     {Array.from({length:10}).map((_,i)=>{
+                     {Array.from({length:8}).map((_,i)=>{
                         return (
-                           <tr key={i} className="border-b border-white/10">
+                           <tr key={i} className="border-b border-white/10 hover:bg-white/5">
                               <td className="py-3 px-4 text-sm text-zinc-300">
-                                 <input type="checkbox" />
+                                 <input type="checkbox" className="size-4 bg-black/20 border border-white/10 rounded" />
                               </td>
                               <td className="py-3 px-4 text-sm text-zinc-300">123145</td>
                               <td className="py-3 px-4 text-sm text-zinc-300">
@@ -56,19 +56,19 @@ export function AttendeeList() {
                         Mostrando 10 de 228 itens
                      </td>
                      <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
-                        <div className="flex gap-8 items-center">
+                        <div className="inline-flex gap-8 items-center">
                            <span>Página 1 de 23</span>
                            <div className="flex gap-1.5">
-                              <button className="bg-black/20 border border-white/10 rounded-md p-1.5 ">
+                              <button className="bg-white/10 border border-white/10 rounded-md p-1.5 ">
                                  <ChevronsLeft className="size-4" />
                               </button>
-                              <button className="bg-black/20 border border-white/10 rounded-md p-1.5 ">
+                              <button className="bg-white/10 border border-white/10 rounded-md p-1.5 ">
                                  <ChevronLeft className="size-4" />
                               </button>
-                              <button className="bg-black/20 border border-white/10 rounded-md p-1.5 ">
+                              <button className="bg-white/10 border border-white/10 rounded-md p-1.5 ">
                                  <ChevronRight className="size-4" />
                               </button>
-                              <button className="bg-black/20 border border-white/10 rounded-md p-1.5 ">
+                              <button className="bg-white/10 border border-white/10 rounded-md p-1.5 ">
                                  <ChevronsRight className="size-4" />
                               </button>
                            </div>
