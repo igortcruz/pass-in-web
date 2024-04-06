88f563727d8cbe54dd6c72fe3bd46cd98b44e93d
igortcruz <igortorres213@gmail.com>
2024-04-05 21:40:06 -0300

    Fix: Ajuste no Search e Fetch URL

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -27,19 +27,28 @@ export function AttendeeList() {
    const [attendees, setAttendees] = useState<Attendee[]>([])
 
    useEffect(()=>{
-      fetch(`http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees?pageIndex=${page - 1}`)
+      const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+
+      url.searchParams.set('pageIndex', String(page-1))
+      if (search.length > 0) {
+         url.searchParams.set('query', search)
+      }
+         
+      
+      fetch(url)
          .then(response => response.json())
          .then(data => {
             setAttendees(data.attendees)
             setTotal(data.total)
             console.log(data)
          })
-   },[page])
+   },[page,search])
 
    const totalPages = Math.ceil(total/10)
 
    function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
       setSearch(event.target.value)
+      setPage(1)
    }
 
    function goToFirstPage(){
@@ -64,9 +73,12 @@ export function AttendeeList() {
             <h1 className="text-2xl font-bold">Participantes</h1>
             <div className="px-3 py-1.5 w-72 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                <Search className="size-4 text-emerald-300"/>
-               <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
+               <input 
+                  onChange={onSearchInputChanged} 
+                  className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" 
+                  placeholder="Buscar participante..." 
+               />
             </div>
-            {search}
          </div>
 
          <Table>
